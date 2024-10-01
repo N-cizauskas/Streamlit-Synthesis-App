082eea51511d691b12cd40705d38067e0b136313
Nicole Cizauskas <73740721+N-cizauskas@users.noreply.github.com>
2024-10-01 13:24:19 +0100

    Add files via upload

--- Interactive_Test.py
+++ Interactive_Test.py
@@ -312,9 +312,9 @@ with tab1:
             
                  # Apply truncation for "Race" and "Age"
                 if col == "Race":
-                    new_sample[col] = np.clip(predicted_value, 1, 4)  # Ensure Race is between 1 and 4
+                    new_sample[col] = np.clip(new_sample, 1, 4)  # Ensure Race is between 1 and 4
                 elif col == "Age":
-                    new_sample[col] = np.clip(predicted_value, 10, 90)  # Ensure Age is between 10 and 90
+                    new_sample[col] = np.clip(new_sample, 10, 90)  # Ensure Age is between 10 and 90
             
             # append the new sample row to the synthetic data 
             synthetic_data = pd.concat([synthetic_data, pd.DataFrame([new_sample])], ignore_index=True)
