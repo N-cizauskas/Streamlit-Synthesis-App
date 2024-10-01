eac48cbb67c3a24138a03c303eacbdecf34adb61
Nicole Cizauskas <73740721+N-cizauskas@users.noreply.github.com>
2024-10-01 13:37:04 +0100

    Add files via upload

--- Interactive_Test.py
+++ Interactive_Test.py
@@ -309,25 +309,25 @@ with tab1:
                 else:
                     X = data.drop(columns=[col]).sample(n=1)
 
-                # Predict the continuous value
+                # predict the continuous value
                 predicted_value = synthesize_data_linear(models[col], X)[0]
         
-                # Round the prediction to the nearest integer
+                # round
                 predicted_value = int(np.round(predicted_value))
         
-                # Apply truncation for "Race" and "Age"
+                # truncation 
                 if col == "Race":
                     new_sample[col] = np.clip(predicted_value, 1, 4)  # Ensure Race is between 1 and 4
                 elif col == "Age":
                     new_sample[col] = np.clip(predicted_value, 10, 90)  # Ensure Age is between 10 and 90
                 else:
-                    new_sample[col] = predicted_value  # For other continuous columns, just assign the rounded value
+                    new_sample[col] = predicted_value  
             
-                # Ensure no NA values are present after filling
+                # ensure no NAs after truncation
                 new_sample[col] = new_sample[col] if pd.notna(new_sample[col]) else fill_missing_values(pd.DataFrame([new_sample]), data)[col].values[0]
 
 
-                # Append the new sample row to the synthetic data
+                # append new synthetic data 
                 synthetic_data = pd.concat([synthetic_data, pd.DataFrame([new_sample])], ignore_index=True)
 
         return synthetic_data
